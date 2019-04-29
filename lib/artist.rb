bfb5c36dd66f8909549725811b75d593ad3f6023
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-29 20:36:05 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,10 +15,12 @@ end
     @songs
   end
 
-  def add_song_by_name(name, genre)
-      song = Song.new(name, genre)
+  def add_song_by_name(name)
+      song = Song.new(name)
       @songs << song
       song.artist = self
     end
+
+    
 
 end
